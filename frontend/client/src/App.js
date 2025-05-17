6925a9cfaf7b92c2d70c90388297e5465e32751e
Brian <brian.ganzler@outlook.com>
2025-05-16 21:09:59 -0400

    Created logout button and adjusted visibility for private links based on useState. Private links are currently not protected.

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,17 +1,30 @@
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import {useState} from "react";
 import Navbar from "./components/Navbar";
 import ROUTES from "./routes"
 
+
 function AppContent() {
     const navigate = useNavigate();
+    const [userID, setUserID] = useState(localStorage.getItem("user_id"));
     const publicLinks = [
         {label: "Home", onClick: () => navigate("/")},
         {label: "Login", onClick: () => navigate("/login")},
         {label: "Create Account", onClick: () => navigate("/create-account")}
-    ]
+    ];
+    const privateLinks = [
+        {label: "Home", onClick: () => navigate("/")},
+        {label: "Dashboard", onClick: () => navigate("/dashboard")},
+        {label: "Logout", onClick: () => {
+            localStorage.removeItem("user_id")
+            setUserID(null)
+            navigate("/");
+        }}
+    ];
+    const linksToUse = userID ? privateLinks : publicLinks;
     return (
         <>
-            <Navbar links={publicLinks}/>
+            <Navbar links={linksToUse}/>
             <Routes>
                 {Object.values(ROUTES).map(({path, element}) => (
                     <Route
