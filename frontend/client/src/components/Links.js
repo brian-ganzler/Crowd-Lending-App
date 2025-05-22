8a68ccd10ed3f2829d99acac0ce82a67e0cc07a3
Brian <brian.ganzler@outlook.com>
2025-05-21 22:40:23 -0400

    Removed unnecessary lines

--- frontend/client/src/components/Links.js
+++ frontend/client/src/components/Links.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Navigate} from 'react-router-dom';
 
+
 export function ProtectedRoute({children}) {
     const userID = localStorage.getItem("user_id")
     return userID ? children : <Navigate to = "/login" />;
